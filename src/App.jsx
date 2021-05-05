a3e15c055551dff4a37960102528f62bf320f9ac
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2021-05-05 15:26:43 +0900

    [feat] 프로젝트 초기 셋팅
    - cra 설정
    - prettierr 적용

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Header from '@components/Header';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <h1>Hello React</h1>
+    </>
+  );
+}
+
+export default App;
