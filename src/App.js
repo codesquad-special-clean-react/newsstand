3741ba560e92e2a4eb83ef76513ed2bab74ba89d
inhyuck <dlsgur1447@gmail.com>
2021-05-06 21:55:17 +0900

    feat: 기본 route 구조

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav>
+        <div>
+          <span>뉴스스탠드 ></span>
+          <Link to="/all"> 전체언론사 </Link> |<Link to="/my"> MY 뉴스</Link>
+        </div>
+        <Route path="/my">
+          <Link to="/my/card">카드형 </Link> |<Link to="/my/list"> 목록형</Link>
+        </Route>
+        <div>이전 / 다음</div>
+      </nav>
+      <Switch>
+        <Route path="/all">전체언론사</Route>
+        <Route path="/my">MY 뉴스</Route>
+        <Route path="/">전체언론사 or MY 뉴스 중 선택해주세요.</Route>
+      </Switch>
+    </Router>
   );
 }
 
