c8a52c0133e50776a2664f61e57e452c982532e8
kimjanghu <kjh921030@gmail.com>
2021-05-09 14:48:01 +0900

    feat: Add suspense in entry point

--- newsstand/src/index.js
+++ newsstand/src/index.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Suspense fallback={<h1>loading...</h1>}>
+      <App />
+    </Suspense>
   </React.StrictMode>,
   document.getElementById("root")
 );
