bff9a45990009ea4aa9ef3b4375c9ffb88596fe6
parkyes90 <parkyes90@gmail.com>
2021-05-10 00:57:31 +0900

    feat: Header Styleing

--- src/components/organisms/header/index.js
+++ src/components/organisms/header/index.js
@@ -1,9 +1,36 @@
-import styled from "styled-components";
-
-const StyledHeader = styled.header``;
+import {
+  Breadcrumb,
+  Divider,
+  Layout,
+  LayoutSelector,
+  Menu,
+  Navigator,
+  Navigators,
+  StyledHeader,
+  Title,
+} from "./styles";
 
 const Header = () => {
-  return <StyledHeader>Header</StyledHeader>;
+  return (
+    <StyledHeader>
+      <Breadcrumb>
+        <Menu>뉴스스탠드</Menu>
+        <Divider>></Divider>
+        <Menu>전체 언론사</Menu>
+      </Breadcrumb>
+      <Title>
+        <span>MY 뉴스</span>
+        <LayoutSelector>
+          <Layout>카드</Layout>
+          <Layout>리스트</Layout>
+        </LayoutSelector>
+      </Title>
+      <Navigators>
+        <Navigator>{"<"}</Navigator>
+        <Navigator>{">"}</Navigator>
+      </Navigators>
+    </StyledHeader>
+  );
 };
 
 export default Header;
---
+++ src/components/organisms/header/styles.js
@@ -0,0 +1,56 @@
+import styled, { css } from "styled-components";
+
+const ClickableMixin = css`
+  cursor: pointer;
+  &:hover {
+    opacity: 0.6;
+  }
+`;
+
+export const FlexDiv = styled.div`
+  display: flex;
+`;
+
+export const StyledHeader = styled.header`
+  display: flex;
+  align-items: center;
+  flex: 1;
+  font-size: 1.3rem;
+  padding: 1rem;
+  border: 1px solid lightgray;
+`;
+
+export const Breadcrumb = styled.div``;
+
+export const Title = styled(FlexDiv)`
+  justify-content: space-around;
+  flex: 1;
+`;
+
+export const LayoutSelector = styled(FlexDiv)``;
+
+export const Navigators = styled(FlexDiv)`
+  margin: inherit;
+`;
+
+export const Navigator = styled.div`
+  ${ClickableMixin};
+  padding: 1rem;
+  border: 1px solid lightgray;
+`;
+
+export const Layout = styled.div`
+  ${ClickableMixin};
+  &:first-child {
+    margin-right: 1rem;
+  }
+`;
+
+export const Divider = styled.span`
+  margin-left: 1rem;
+  margin-right: 1rem;
+`;
+
+export const Menu = styled.span`
+  ${ClickableMixin};
+`;
