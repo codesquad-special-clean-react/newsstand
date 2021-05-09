caeeb6ea92d55b24732195eb2c532e59a8dcab5c
kimjanghu <kjh921030@gmail.com>
2021-05-10 01:04:37 +0900

    chore: Add constants

---
+++ newsstand/src/utils/constants.js
@@ -0,0 +1,15 @@
+export const NAV_NAME = {
+  MY_NEWS: "MY 뉴스",
+  ENTRY_PRESS: "전체 언론사",
+  CARD_TYPE: "cardType",
+  LIST_TYPE: "listType",
+};
+
+export const ROUTE = {
+  HOME: "/",
+  MY_NEWS: "/mynews",
+};
+
+export const FETCH_TYPE = {
+  NEWS: "NEWS",
+};
