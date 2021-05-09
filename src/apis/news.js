a36e25a5742f0285d2d1e9656ac63917800fef1d
parkyes90 <parkyes90@gmail.com>
2021-05-10 02:59:32 +0900

    feat: 뉴스 수정 API 생성

--- src/apis/news.js
+++ src/apis/news.js
@@ -3,3 +3,13 @@ const URL = "http://localhost:3001/news";
 export const fetchNewsList = () => {
   return fetch(URL);
 };
+
+export const patchNews = ({ newsId, params }) => {
+  return fetch(`${URL}/${newsId}`, {
+    method: "PATCH",
+    body: JSON.stringify(params),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+};
