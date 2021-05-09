c09494ddab001c679d403de9c245308e41315f5e
parkyes90 <parkyes90@gmail.com>
2021-05-10 01:50:23 +0900

    feat: Add 이전 && 이후 기능

--- src/components/organisms/header/index.js
+++ src/components/organisms/header/index.js
@@ -9,8 +9,42 @@ import {
   StyledHeader,
   Title,
 } from "./styles";
+import { useRecoilState } from "recoil";
+import { currentNewsState, newsListState } from "../../../recoils/atoms";
 
 const Header = () => {
+  const [newsList] = useRecoilState(newsListState);
+  const [currentNews, setCurrentNews] = useRecoilState(currentNewsState);
+
+  const raiseError = () => {
+    if (newsList.length < 1) {
+      throw new Error("뉴스리스트를 불러오지 못했습니다.");
+    }
+  };
+
+  const handleClickNext = () => {
+    raiseError();
+    if (!currentNews) {
+      setCurrentNews(newsList[0]);
+    } else {
+      const nextIndex = newsList.indexOf(currentNews) + 1;
+      const newIndex = nextIndex >= newsList.length ? 0 : nextIndex;
+      setCurrentNews(newsList[newIndex]);
+    }
+  };
+
+  const handleClickPrevious = () => {
+    raiseError();
+    const initialIndex = newsList.length - 1;
+    if (!currentNews) {
+      setCurrentNews(newsList[initialIndex]);
+    } else {
+      const previousIndex = newsList.indexOf(currentNews) - 1;
+      const newIndex = previousIndex < 0 ? initialIndex : previousIndex;
+      setCurrentNews(newsList[newIndex]);
+    }
+  };
+
   return (
     <StyledHeader>
       <Breadcrumb>
@@ -26,8 +60,8 @@ const Header = () => {
         </LayoutSelector>
       </Title>
       <Navigators>
-        <Navigator>{"<"}</Navigator>
-        <Navigator>{">"}</Navigator>
+        <Navigator onClick={handleClickPrevious}>{"<"}</Navigator>
+        <Navigator onClick={handleClickNext}>{">"}</Navigator>
       </Navigators>
     </StyledHeader>
   );
